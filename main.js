da0aab1214011f8e60d3b6618ef30c7e59697146
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-02-10 11:05:03 -0700

    done

--- main.js
+++ main.js
@@ -12,12 +12,18 @@ function btnClicked() {
     let vertexCX = document.getElementById('xC').value;
     let vertexCY = document.getElementById('yC').value;
 
-    // Calculate % Output Distance
+    // Variables for side lengths
+    let AB = dist(vertexAX, vertexAY, vertexBX, vertexBY);
+    let AC = dist(vertexAX, vertexAY, vertexCX, vertexCY);
+    let BC = dist(vertexBX, vertexBY, vertexCX, vertexCY);
+
+    // Calculate and Output Distance and Perimeter
     document.getElementById('dist-ab').innerHTML = dist(vertexAX, vertexAY, vertexBX, vertexBY);
     document.getElementById('dist-ac').innerHTML = dist(vertexAX, vertexAY, vertexCX, vertexCY);
     document.getElementById('dist-bc').innerHTML = dist(vertexBX, vertexBY, vertexCX, vertexCY);
+    document.getElementById('perimeter').innerHTML = AB + AC + BC;
 }
 
 function dist(x1, y1, x2, y2) {
-    return x1 * 2
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
